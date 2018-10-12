0776dbdbaa98fefa3093c570a2e57977101b4035
ChaeJuYoung <jyc132@naver.com>
2018-10-12 14:49:53 +0900

    Juyoung Commit
    Juyoung Commit

---
+++ Juyoung/mini/fireinit.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+    apiKey: "AIzaSyAq5vyhPSVxhoaOd55ewVb4sh3AvXNw4yw",
+    authDomain: "pi-iot-1b450.firebaseapp.com",
+    databaseURL: "https://pi-iot-1b450.firebaseio.com",
+    projectId: "pi-iot-1b450",
+    storageBucket: "pi-iot-1b450.appspot.com",
+    messagingSenderId: "1144253737"
+};
+firebase.initializeApp(config);
